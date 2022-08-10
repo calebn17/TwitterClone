846be3496963588bc33d0e282a8c1e5f7c36af2a
calebn17 <ccngai17@utexas.edu>
2022-08-10 07:14:20 -0700

    main actors

--- TwitterClone/TweetView/TweetViewModel.swift
+++ TwitterClone/TweetView/TweetViewModel.swift
@@ -22,10 +22,10 @@ struct TweetModel: Codable {
 
 struct TweetViewModel {
     
-    var profilePictureURL = Observable<URL?>(nil)
+    @MainActor var profilePictureURL = Observable<URL?>(nil)
     var currentUser: User { return DatabaseManager.shared.currentUser }
     
-    func fetchProfilePictureURL(tweet: TweetModel) async throws {
+    @MainActor func fetchProfilePictureURL(tweet: TweetModel) async throws {
         let user = User(
             id: nil,
             userName: tweet.username,
